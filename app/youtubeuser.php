ad9e34066413bd4a9d289bbaeef9b0ef1d601b42
Tanjavalhoj <Tanja.valhoj@gmail.com>
2018-12-01 17:33:15 +0100

    rettet youtubeuser.php

--- app/youtubeuser.php
+++ app/youtubeuser.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class youtubeuser extends Model
 {
     //
+
+    protected $primaryKey = "UserId";
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'YoutubeUser';
 }
